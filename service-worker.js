70911177620b22bf6672952ccbc89bc49d7ae6ec
adonyposadas-cloud <adonyposadas@gmail.com>
2026-04-27 07:07:04 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
  *  - Invalidación por versión: al subir cambios, incrementa CACHE_VERSION.
  */
 
-const CACHE_VERSION = 'tecnimax-taller-v1.6.0';
+const CACHE_VERSION = 'tecnimax-taller-v1.6.1';
 const APP_SHELL = [
   './',
   './index.html',
